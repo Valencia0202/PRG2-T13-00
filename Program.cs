8ece8b11100461e5ca081cdee6d5cfe97e755389
Valencia <s10266236@connect.np.edu.sg>
2025-01-09 16:46:01 +0800

    hi

--- Program.cs
+++ Program.cs
@@ -1,15 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-<<<<<<< HEAD
-<<<<<<< HEAD
-Console.WriteLine("Hello!kkkkkkkkkkkkkkkkkkk");
-=======
-Console.WriteLine("Hello");
-biiittcchhhhhhjuhuhujujuj commit.
->>>>>>> cc2f673bcbedca435da02cc5bd6fbad98fe145ad
-=======
-Console.WriteLine("Hello");
-biiittcchhhhhhjuhuhujujuj commit.
-    hgfghjkh,
->>>>>>> a15a7f8d458a6a2273b0d61eb867df7ac54489d9
-
-TRHOW HAND
+Console.WriteLine("hi");
