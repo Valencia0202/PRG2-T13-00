fc9e8c2ad121c61c896701ae13a782ec5add2a0f
Valencia <s10266236@connect.np.edu.sg>
2025-01-16 15:29:44 +0800

    clear

--- Program.cs
+++ Program.cs
@@ -1,6 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello");
 
-Console.WriteLine("hi");
-Console.WriteLine("hi BITCH");
-Console.WriteLine("hi G");  
